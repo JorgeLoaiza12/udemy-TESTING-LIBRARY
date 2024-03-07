bed677c6467511e2dee150fc875df1fa90e64af0
Jorge Loaiza <jorgeloaiza12@gmail.com>
2024-03-06 23:22:38 -0300

    introduction course

--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -4,5 +4,24 @@ import App from "./App";
 test("App contains correct heading", () => {
   render(<App />);
   const headingElement = screen.getByText(/learn react/i);
-  expect(headingElement).toBeInTheDocument();
+  const headingElement1 = screen.getByRole("heading", { name: /learn 2/i });
+  expect(headingElement).toBeInTheDocument(); // type of assertion -> matcher
+  expect(headingElement1).toBeInTheDocument(); // type of assertion -> matcher
 });
+
+
+// Assertions
+// expect(element.textContent).toBe("Hello")
+// expect(elementsArray).toHaveLength(7)
+
+// DOM-based matchers
+// toBeVisible() or toBeChecked()
+
+/*
+React Testing Library:
+- Renders components into simulated DOM
+- Makes simulated DOM available for assertions and interactions
+
+Needs a test runner:
+- Find tests, runs them, make assertions
+*/
